927c83fabbf26a8ee6a77681714c8bf1cf60e363
Next <example@email.com>
2023-09-30 21:48:36 +0300

    добавил кнопку

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, HTMLResponse 
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+import random
 
 app = FastAPI(
     title="Homchik"
@@ -12,9 +14,11 @@ app.mount("/static", StaticFiles(directory="static"), name = "static")
 
 templates = Jinja2Templates(directory="templates")
 
+rand_num = ""
+
 @app.get("/", response_class=HTMLResponse)
 async def read_home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("home.html", {"request": request,  "rand_num": rand_num})
 
 @app.get("/about", response_class=HTMLResponse)
 async def read_about(request: Request):
@@ -23,3 +27,16 @@ async def read_about(request: Request):
 @app.get("/contact", response_class=HTMLResponse)
 async def read_contact(request: Request):
     return templates.TemplateResponse("contact.html", {"request": request})
+
+"""
+@app.get("/")
+async def get_random_number(request: Request):
+    rand_num = random.randint(0, 100)
+    return templates.TemplateResponse("home.html", {"request": request, "rand_num": rand_num})
+"""
+@app.get("/button")
+async def get_random_number():
+    global rand_num
+    rand_num = random.randint(0, 100)  # Генерируем случайное число от 1 до 100
+    return RedirectResponse(url="/", status_code=303)
+
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == '__main__':
     uvicorn.run(
         "app:app",
-        host='0.0.0.0',
-        port=10000,
+        host='127.0.0.1', #0.0.0.0 #127.0.0.1
+        port=8000,    #10000   #8000
         reload=True
     )
